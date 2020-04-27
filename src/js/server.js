8c1b5c6cf8b18d89cdf43500dd2319ef196479d8
EricNguyen98 <60360013+EricNguyen98@users.noreply.github.com>
2020-04-27 05:20:51 -0400

    init

---
+++ src/js/server.js
@@ -0,0 +1,45 @@
+// These are our required libraries to make the server work.
+
+const express = require('express');
+const fetch = require('node-fetch');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+function processDataForFrontEnd(req, res) {
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/baxv-ntrj.json'; // Enter the URL for the data you would like to retrieve here
+
+  // Your Fetch API call starts here
+  // Note that at no point do you "return" anything from this function -
+  // it instead handles returning data to your front end at line 34.
+    fetch(baseURL)
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data);
+        res.send({ data: data }); // here's where we return data to the front end
+      })
+      .catch((err) => {
+        console.log(err);
+        res.redirect('/error');
+      });
+}
+
+// Syntax change - we don't want to repeat ourselves,
+// or we'll end up with spelling errors in our endpoints.
+// 
+app.route('/api')
+  .get((req, res) => {processDataForFrontEnd(req, res)})
+  .post((req, res) => {
+    console.log("/api post request", req.body);
+    res.send('your request was successful'); // simple mode
+  })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
