d42cd9432ef42b78cf388cb11c5a9d468aa1fd4a
EricNguyen98 <60360013+EricNguyen98@users.noreply.github.com>
2020-04-27 09:51:24 -0400

    server update

--- src/js/server.js
+++ src/js/server.js
@@ -3,43 +3,138 @@
 const express = require('express');
 const fetch = require('node-fetch');
 
-
 const app = express();
 const port = process.env.PORT || 3000;
+const valid = ['HEALTH', 'FIRE', 'PSC', 'DER', 'POLICE', 'SHERIFF', 'COUNTY EXECUTIVE', 'DPWT', 'COURTS', 'OCS', 'SOIL CONSERVATION', 'OHRM', 'OIT'];
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-
-
+app.use(express.static('src'));
 
 function processDataForFrontEnd(req, res) {
   const baseURL = 'https://data.princegeorgescountymd.gov/resource/baxv-ntrj.json'; // Enter the URL for the data you would like to retrieve here
-
-  // Your Fetch API call starts here
-  // Note that at no point do you "return" anything from this function -
-  // it instead handles returning data to your front end at line 34.
+  const agency = req.body.agency.toUpperCase();
+  console.log(agency);
+  const packet = ({ 'agencies': [] });
+  if (!valid.includes(agency)) {
+    packet.agencies.push({
+      'Error Message': `Sorry we could not find a PG County Office for ${req.body.agency}`,
+      'Agency': valid
+    });
+  } else {
     fetch(baseURL)
       .then((r) => r.json())
       .then((data) => {
-        console.log(data);
-        res.send({ data: data }); // here's where we return data to the front end
+        // process data
+        for (let i = 0; i < data.length; i++ ) {
+          if (data[i].agency === agency) {
+            const office = data[i];
+            const { address } = office;
+            const street = JSON.parse(address.human_address);
+            packet.agencies.push({
+              'description': office.description,
+              'human_address': `${street.address} ${street.city} ${street.state} ${street.zip}`,
+              'city': street.city,
+              'agency': office.agency,
+              'long': office.address.longitude,
+              'lat': office.address.latitude
+            });
+          }
+        }
+        console.log(packet);
+        res.json(packet); // here's where we return data to the front end
       })
       .catch((err) => {
         console.log(err);
         res.redirect('/error');
       });
+  }
+}
+function processDataForList(req, res) {
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/baxv-ntrj.json'; // Enter the URL for the data you would like to retrieve here
+  const agency = req.body.agency.toUpperCase();
+  console.log(agency);
+  const packet = ({ 'agencies': [] });
+  if (!valid.includes(agency)) {
+    packet.agencies.push({
+      'Error_Message': `Sorry we could not find a PG County Office for ${req.body.agency}`,
+      'Agency': valid
+    });
+    res.json(packet);
+  } else {
+    fetch(baseURL)
+      .then((r) => r.json())
+      .then((data) => {
+        // process data
+        for (let i = 0; i < data.length; i++ ) {
+          if (data[i].agency === agency) {
+            const office = data[i];
+            const { address } = office;
+            const street = JSON.parse(address.human_address);
+            packet.agencies.push({
+              'description': office.description,
+              'human_address': `${street.address} ${street.city}, ${street.state} ${street.zip}`,
+              'city': street.city,
+              'agency': office.agency
+            });
+          }
+        }
+        console.log(packet);
+        res.json(packet); // here's where we return data to the front end
+      })
+      .catch((err) => {
+        console.log(err);
+        res.redirect('/error');
+      });
+  }
 }
 
+function retrieveAll(res) {
+  const baseURL = 'https://data.princegeorgescountymd.gov/resource/baxv-ntrj.json'; // Enter the URL for the data you would like to retrieve here
+  const packet = ({ 'agencies': [] });
+
+  fetch(baseURL)
+    .then((r) => r.json())
+    .then((data) => {
+      // process data
+      for (let i = 0; i < data.length; i++ ) {
+        const office = data[i];
+        const { address } = office;
+        const street = JSON.parse(address.human_address);
+        packet.agencies.push({
+          'description': office.description,
+          'human_address': `${street.address} ${street.city}, ${street.state} ${street.zip}`,
+          'city': street.city,
+          'agency': office.agency,
+          'long': office.address.longitude,
+          'lat': office.address.latitude
+        });
+      }
+      console.log(packet);
+      res.json(packet); // here's where we return data to the front end
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
+}
 // Syntax change - we don't want to repeat ourselves,
 // or we'll end up with spelling errors in our endpoints.
-// 
-app.route('/api')
-  .get((req, res) => {processDataForFrontEnd(req, res)})
-  .post((req, res) => {
-    console.log("/api post request", req.body);
-    res.send('your request was successful'); // simple mode
+//
+app
+  .route('/api')
+  .get((req, res) => {
+    console.log('all');
+    retrieveAll(res);
   })
+  .post((req, res) => {
+    console.log('list');
+    processDataForList(req, res);
+  })
+  .put((req, res) => {
+    console.log('map');
+    processDataForFrontEnd(req, res);
+  });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
